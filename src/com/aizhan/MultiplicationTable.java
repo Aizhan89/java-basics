6d0a23934e7a6d8611de8ca8adbc9913c300cac0
Aizhan Mambetalieva <aijan312kg@gmail.com>
2021-04-27 16:25:20 +0300

    Adding more method for multiplication table

--- src/com/aizhan/MultiplicationTable.java
+++ src/com/aizhan/MultiplicationTable.java
@@ -8,4 +8,14 @@ public class MultiplicationTable {
             System.out.printf("%d * %d = %d",5, i, 5*i).println();
         }
     }
+    public void print(int table) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d = %d", table, i, table * i).println();
+        }
+    }
+    public void print(int table, int from, int to) {
+        for (int i = from; i <= to; i++) {
+            System.out.printf("%d * %d = %d", table, i, table * i).println();
+        }
+    }
 }
--- src/com/aizhan/MultiplicationTableRunner.java
+++ src/com/aizhan/MultiplicationTableRunner.java
@@ -3,6 +3,8 @@ package com.aizhan;
 public class MultiplicationTableRunner {
     public static void main(String[] args){
         MultiplicationTable table = new MultiplicationTable();
-        table.print();
+       // table.print();
+       // table.print(12);
+        table.print(5,11,20);
     }
 }
