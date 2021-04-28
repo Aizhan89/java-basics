16638719de12f2e215513b2aad44d308d8606500
Aizhan Mambetalieva <aijan312kg@gmail.com>
2021-04-28 10:25:48 +0300

    Refactoring multiplication table

--- src/com/aizhan/MultiplicationTable.java
+++ src/com/aizhan/MultiplicationTable.java
@@ -3,16 +3,20 @@ package com.aizhan;
 public class MultiplicationTable {
     //5*1 = 5
     //5*10 = 50
-    public void print(){
-        for(int i = 1; i<=10; i++) {
-            System.out.printf("%d * %d = %d",5, i, 5*i).println();
-        }
+    public void print() {
+        print(5);
+        //for(int i = 1; i<=10; i++) {
+        //  System.out.printf("%d * %d = %d",5, i, 5*i).println();
     }
+
+    //}
     public void print(int table) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.printf("%d * %d = %d", table, i, table * i).println();
-        }
-    }
+        print(table, 1, 10);
+        //  for (int i = 1; i <= 10; i++) {
+        //   System.out.printf("%d * %d = %d", table, i, table * i).println();
+      }
+
+   //}
     public void print(int table, int from, int to) {
         for (int i = from; i <= to; i++) {
             System.out.printf("%d * %d = %d", table, i, table * i).println();
